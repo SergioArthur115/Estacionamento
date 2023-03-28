dc7ff6d6250e867567d69e1de95121386421b0d1
Chrigor <chrigorgamer@gmail.com>
2023-03-28 10:25:26 -0300

    Criação dos menus

--- src/estacionamento/Estacionamento.java
+++ src/estacionamento/Estacionamento.java
@@ -1,21 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estacionamento;
 
-/**
- *
- * @author 182120042
- */
 public class Estacionamento {
+    public static void Menu() { // Inicio MENU
+        System.out.println("+--------------------+");
+        System.out.println("|   ESTACIONAMENTO   |");
+        System.out.println("+--------------------+");
+        System.out.println("|                    |");
+        System.out.println("| 1 - Ger. Cliente   |");
+        System.out.println("| 2 - Ger. Carro     |");
+        System.out.println("| 1 - Listar dados   |");
+        System.out.println("|                    |");
+        System.out.println("+--------------------+");
+        System.out.print("  Escolha uma opção: ");
+    }// Fim MENU
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void SubMenu(int op) {// Inicio SUBMENU
+        String tpger = null;
+        switch (op) {
+            case 1:
+                tpger = "Cliente";
+                break;
+            case 2:
+                tpger = "Carro";
+                break;
+        }
+        System.out.println("+--------------------+");
+        System.out.println("|   GERENCIAMENTO    |");
+        System.out.println("+--------------------+");
+        System.out.println("|                    |");
+        System.out.println("| 1 - Cadastrar " + tpger + "|");
+        System.out.println("| 1 - Editar " + tpger + "|");
+        System.out.println("| 1 - Listar " + tpger + "|");
+        System.out.println("| 1 - Remover " + tpger + "|");
+        System.out.println("+--------------------+");
+        System.out.print("  Escolha uma opção: ");
+    }// Fim SUBMENU
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
