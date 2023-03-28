94eb097472b0ca7c6484d8e318deaf8570665efa
SergioArthur115@gmail.com <182120042@senacinformatica.br>
2023-03-28 11:27:29 -0300

    criação do validadores de cpf telefone e renavam

--- src/util/Validadores.java
+++ src/util/Validadores.java
@@ -5,10 +5,149 @@
  */
 package util;
 
+import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author 182120042
  */
 public class Validadores {
-    
+
+    public static boolean isCPF(String CPF) {
+        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+        if (CPF.equals("00000000000")
+                || CPF.equals("11111111111")
+                || CPF.equals("22222222222") || CPF.equals("33333333333")
+                || CPF.equals("44444444444") || CPF.equals("55555555555")
+                || CPF.equals("66666666666") || CPF.equals("77777777777")
+                || CPF.equals("88888888888") || CPF.equals("99999999999")
+                || (CPF.length() != 11)) {
+            return (false);
+        }
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+            // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+                // converte o i-esimo caractere do CPF em um numero:
+                // por exemplo, transforma o caractere '0' no inteiro 0
+                // (48 eh a posicao de '0' na tabela ASCII)
+                num = (int) (CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig10 = '0';
+            } else {
+                dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+            }
+            // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig11 = '0';
+            } else {
+                dig11 = (char) (r + 48);
+            }
+
+            // Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10))) {
+                return (true);
+            } else {
+                return (false);
+            }
+        } catch (InputMismatchException erro) {
+            return (false);
+        }
+    }
+
+    public static String imprimeCPF(String CPF) {
+        return (CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "."
+                + CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
+    }
+
+    public static boolean validarTelefone(String telefone) {
+        String regex = "\\(?\\d{2}\\)?\\s?\\d{4,5}\\-?\\d{4}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(telefone);
+        return matcher.matches();
+    }
+
+    public boolean validarRenavam(String renavam) {
+        // Pegando como exemplo o renavam = 639884962
+
+        // Completa com zeros a esquerda se for no padrao antigo de 9 digitos
+        // renavam = 00639884962
+        if (renavam.matches("^([0-9]{9})$")) {
+            renavam = "00" + renavam;
+        }
+
+        // Valida se possui 11 digitos pos formatacao
+        if (!renavam.matches("[0-9]{11}")) {
+            return false;
+        }
+
+        // Remove o digito (11 posicao)
+        // renavamSemDigito = 0063988496
+        String renavamSemDigito = renavam.substring(0, 10);
+
+        // Inverte os caracteres (reverso)
+        // renavamReversoSemDigito = 69488936
+        String renavamReversoSemDigito = new StringBuffer(renavamSemDigito).reverse().toString();
+
+        int soma = 0;
+
+        // Multiplica as strings reversas do renavam pelos numeros multiplicadores
+        // para apenas os primeiros 8 digitos de um total de 10
+        // Exemplo: renavam reverso sem digito = 69488936
+        // 6, 9, 4, 8, 8, 9, 3, 6
+        // * * * * * * * *
+        // 2, 3, 4, 5, 6, 7, 8, 9 (numeros multiplicadores - sempre os mesmos [fixo])
+        // 12 + 27 + 16 + 40 + 48 + 63 + 24 + 54
+        // soma = 284
+        for (int i = 0; i < 8; i++) {
+            Integer algarismo = Integer.parseInt(renavamReversoSemDigito.substring(i, i + 1));
+            Integer multiplicador = i + 2;
+            soma += algarismo * multiplicador;
+        }
+
+        // Multiplica os dois ultimos digitos e soma
+        soma += Character.getNumericValue(renavamReversoSemDigito.charAt(8)) * 2;
+        soma += Character.getNumericValue(renavamReversoSemDigito.charAt(9)) * 3;
+
+        // mod11 = 284 % 11 = 9 (resto da divisao por 11)
+        int mod11 = soma % 11;
+
+        // Faz-se a conta 11 (valor fixo) - mod11 = 11 - 9 = 2
+        int ultimoDigitoCalculado = 11 - mod11;
+
+        // ultimoDigito = Caso o valor calculado anteriormente seja 10 ou 11, transformo ele em 0
+        // caso contrario, eh o proprio numero
+        ultimoDigitoCalculado = (ultimoDigitoCalculado >= 10 ? 0 : ultimoDigitoCalculado);
+
+        // Pego o ultimo digito do renavam original (para confrontar com o calculado)
+        int digitoRealInformado = Integer.valueOf(renavam.substring(renavam.length() - 1, renavam.length()));
+
+        // Comparo os digitos calculado e informado
+        if (ultimoDigitoCalculado == digitoRealInformado) {
+            return true;
+        }
+        return false;
+    }
 }
